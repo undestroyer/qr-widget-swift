26425ea2fdd8898733a85a7af18a8d09629eb306
Dmitriy Sazonov <undestroyer@gmail.com>
2021-12-19 21:31:28 +0700

    Initial Commit

---
+++ QrWidget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QrWidget
+//
+//  Created by Dmitriy Sazonov on 19.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
